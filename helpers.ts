92f1d1f638db4de3b14e9533f783229d6aa71394
Pascal Luther <pascal@luther.ch>
2021-07-14 21:41:38 +0200

    added displayNotification helper

--- helpers.ts
+++ helpers.ts
@@ -1,8 +1,12 @@
 /** HELPERS **/
 // Firebase
-export { unwrapQuerySnapshot } from "./helpers/firebase/firestore";
-export { checkIfUserHasMinimumRole } from "./helpers/firebase/functions-admin";
+export { unwrapQuerySnapshot } from './helpers/firebase/firestore'
+export { checkIfUserHasMinimumRole } from './helpers/firebase/functions-admin'
 
 // Ionic
-export { openModal } from "./helpers/ionic/modalHelpers";
-export { handleError, handleSuccess } from "./helpers/ionic/toastHandler";
+export { openModal } from './helpers/ionic/modalHelpers'
+export {
+  handleError,
+  handleSuccess,
+  displayNotification,
+} from './helpers/ionic/toastHandler'
--- helpers/ionic/toastHandler.ts
+++ helpers/ionic/toastHandler.ts
@@ -1,36 +1,53 @@
-import { toastController } from "@ionic/vue";
+import { toastController } from '@ionic/vue'
 
 export async function handleError(e: any, message?: string) {
-  console.error(e);
+  console.error(e)
   const toast = await toastController.create({
-    header: "Something went wrong.",
+    header: 'Something went wrong.',
     message: message,
     duration: 5000,
-    position: "top",
-    color: "danger",
+    position: 'top',
+    color: 'danger',
     buttons: [
       {
-        text: "Close",
-        role: "cancel",
+        text: 'Close',
+        role: 'cancel',
       },
     ],
-  });
-  return toast.present();
+  })
+  return toast.present()
 }
 
 export async function handleSuccess(message?: string, header?: string) {
   const toast = await toastController.create({
-    header: header || "Succees!",
+    header: header || 'Succees!',
     message: message,
     duration: 5000,
-    position: "top",
-    color: "success",
+    position: 'top',
+    color: 'success',
     buttons: [
       {
-        text: "Close",
-        role: "cancel",
+        text: 'Close',
+        role: 'cancel',
       },
     ],
-  });
-  return toast.present();
+  })
+  return toast.present()
+}
+
+export async function displayNotification(message?: string, header?: string) {
+  const toast = await toastController.create({
+    header: header,
+    message: message,
+    duration: 10000,
+    position: 'top',
+    color: 'primary',
+    buttons: [
+      {
+        text: 'Close',
+        role: 'cancel',
+      },
+    ],
+  })
+  return toast.present()
 }
