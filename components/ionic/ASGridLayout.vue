d6244c49aafae95eddd1a55d40f2a2652efad92c
Pascal Luther <pascal@luther.ch>
2021-07-31 11:25:20 +0200

    feat: added ASGridLayout

---
+++ components/ionic/ASGridLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <ASBaseLayout>
+    <ion-grid class="_iosMargin">
+      <ion-row>
+        <ion-col
+          size="12"
+          size-sm="10"
+          offset-sm="1"
+          size-md="10"
+          offset-md="1"
+          size-lg="6"
+          offset-lg="3"
+        >
+          <h1 v-if="title" class="pageTitle">
+            {{ title }}
+          </h1>
+          <slot />
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+  </ASBaseLayout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ASBaseLayout from './ASBaseLayout.vue'
+import { IonGrid, IonRow, IonCol } from '@ionic/vue'
+
+export default defineComponent({
+  props: ['title'],
+  components: {
+    ASBaseLayout,
+    IonGrid,
+    IonRow,
+    IonCol,
+  },
+})
+</script>
+
+<style scoped>
+.pageTitle {
+  color: white;
+  font-size: 35px;
+  text-align: center;
+}
+</style>
