1c7bd49ecf6a430f267ad3e0b28e8b4910c2c82f
lupas <4020037+lupas@users.noreply.github.com>
2022-06-19 15:53:46 +0200

    cicd(npx): Moved to node-ts

--- packages/cicd/lib/ionic-change-app/helpers.ts
+++ packages/cicd/lib/ionic-change-app/helpers.ts
@@ -7,13 +7,13 @@ function start(title: string) {
 const packagesDir = `${process.cwd()}/..`
 const appPath = `${packagesDir}/app`
 const cicdDir = `${packagesDir}/cicd-utils`
-const moduleSrcPath = `${__dirname}/../../src`
+const moduleSrcPath = `${__dirname}/../../lib`
 const templatesPath = `${moduleSrcPath}/ionic-change-app/_templates`
 
 // CICD Config
 async function getConfig() {
-  const defaultSettings ={
-    changeSitemap: true
+  const defaultSettings = {
+    changeSitemap: true,
   }
 
   const configPath = `${cicdDir}/config.json`
@@ -27,19 +27,19 @@ async function getConfig() {
 }
 
 async function getOldAppLocalConfig() {
-  const oldConfigPath = `${appPath}/src/assets/dyn/appLocalConfig.json`
+  const oldConfigPath = `${appPath}/src/assets/dyn/appLocalConfig.ts`
   try {
-    const configJson = await import(oldConfigPath)
-    return configJson.default
+    const configTs = await import(oldConfigPath)
+    return configTs.default
   } catch (e) {
     return null
   }
 }
 
 async function getAppLocalConfig(appId: string) {
-  const configPath = `${cicdDir}/apps/${appId}/assets/appLocalConfig.json`
-  const configJson = await import(configPath)
-  return configJson.default
+  const configPath = `${cicdDir}/apps/${appId}/assets/appLocalConfig.ts`
+  const configTs = await import(configPath)
+  return configTs.default
 }
 
 export {
