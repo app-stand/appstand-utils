777d5e33eaccb6859f0660c33f081c3d5e20a60c
lupas <4020037+lupas@users.noreply.github.com>
2022-08-01 14:26:14 +0200

    smalls 2.3.19

--- packages/cicd/lib/ionic-change-app/change-app/index.ts
+++ packages/cicd/lib/ionic-change-app/change-app/index.ts
@@ -73,7 +73,7 @@ export default async function main(
     start('changeRobots')
     changeRobots()
   } catch (e) {
-    handleError('changeApp', e)
+    handleError('changeRobots', e)
   }
 
   try {
