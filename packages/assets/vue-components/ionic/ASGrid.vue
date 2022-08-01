966689e5340cab9f834a1a8a32a235bdff531b2f
lupas <4020037+lupas@users.noreply.github.com>
2022-08-01 13:22:07 +0200

    small types fixes & deps

--- packages/assets/vue-components/ionic/ASGrid.vue
+++ packages/assets/vue-components/ionic/ASGrid.vue
@@ -3,12 +3,12 @@
     <ion-row>
       <ion-col
         size="12"
-        :size-sm="fullWidth ? 12 : 10"
-        :offset-sm="fullWidth ? 0 : 1"
-        :size-md="fullWidth ? 12 : 10"
-        :offset-md="fullWidth ? 0 : 1"
-        :size-lg="fullWidth ? 12 : 6"
-        :offset-lg="fullWidth ? 0 : 3"
+        :size-sm="fullWidth ? '12' : '10'"
+        :offset-sm="fullWidth ? '0' : '1'"
+        :size-md="fullWidth ? '12' : '10'"
+        :offset-md="fullWidth ? '0' : '1'"
+        :size-lg="fullWidth ? '12' : '6'"
+        :offset-lg="fullWidth ? '0' : '3'"
       >
         <slot name="abovetitle" />
         <h1 v-if="title" class="pageTitle">
